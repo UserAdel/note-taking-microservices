a0a4c21c4ccbcf442a34f88a1e62e0d1fad02eef
adel hany <adel58420@gmail.com>
2025-11-27 00:10:56 +0200

    test: add notesService Jest Tests

---
+++ services/note-service/jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src", "<rootDir>/tests"],
+  testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test|tests).ts"],
+  transform: {
+    "^.+\\.ts$": ["ts-jest", {
+      tsconfig: {
+        types: ["node", "jest"]
+      }
+    }],
+  },
+  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts", "!src/index.ts"],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "html"],
+  setupFilesAfterEnv: ["<rootDir>/tests/setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^@shared/(.*)$": "<rootDir>/../../shared/$1",
+  },
+  testTimeout: 10000,
+  clearMocks: true,
+  restoreMocks: true,
+  verbose: true,
+};
